82ac3ad0e6f4416c214602f88a5a9390c11255ce
Luis-Emilio <123605150+ElPoteRD@users.noreply.github.com>
2025-02-05 20:55:05 -0400

    [Configuracion]: Instalacion de Shadcn.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <>
       <h1 className="m-5 text-3xl text-red-500">Hello</h1>
+      <Button>Click me</Button>
     </>
   )
 }
